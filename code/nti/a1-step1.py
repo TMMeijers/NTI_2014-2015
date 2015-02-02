286650acf38a16ab42f2595c31b198e8fb859376
markus <mama@mia.it>
2015-02-02 21:03:07 +0100

    added skeleton shit

--- code/nti/a1-step1.py
+++ code/nti/a1-step1.py
@@ -7,13 +7,28 @@ Created on Mon Feb  2 20:34:12 2015
 
 from argparse import ArgumentParser
 
+def read_file(file_in, n):
+    """
+    reads file_in line by line.
+    
+    returns: map of n-grams with frequency count
+    """
+
+    n_grams_frequency = {}    
+    
+    with open(file_in) as f:
+        for line in f:
+            print line
+            
+    return n_grams_frequency
+    
 if __name__ == "__main__":
     # here code for program
     
     parser = ArgumentParser(description='Assignment 1, Step 1')
-    parser.add_argument('file')
+    parser.add_argument('input_file')
     parser.add_argument('-n', dest='n', type=int, help='Length of word-sequences to process (n-grams)')
     parser.add_argument('-m', dest='m', type=int, default=None, help='Number of n-grams to show in output')
-    
     args = parser.parse_args()
-    print args
+    
+    n_grams_frequency = read_file(args.input_file, args.n)
