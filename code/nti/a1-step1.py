2b9ab72d7cd00cfef48f25d521ca951d1bfe6836
Cornelis Boon <killazelda@gmail.com>
2015-02-03 18:06:17 +0100

    Works now for n > 1. Also can be run from terminal like > python a1-step1.py data/test_file.txt -n 2 -m 10

--- code/nti/a1-step1.py
+++ code/nti/a1-step1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon Feb  2 20:34:12 2015
@@ -7,28 +9,40 @@ Created on Mon Feb  2 20:34:12 2015
 
 from argparse import ArgumentParser
 from collections import OrderedDict
+from string import join
+
+
+def make_grams(words, n):
+	n_grams = []
+	for i in range(len(words)-n+1):
+		n_grams.append(join(words[i:i+n]))
+	return n_grams
+
 
 def read_file(file_in, n):
-    """
-    reads file_in line by line.
-    
-    returns: map of n-grams with frequency count
-    """
-
-    n_grams_frequency = {}    
-    
-    with open(file_in) as f:
-        for line in f:
-            splitted_line = line.split()
-            #print splitted_line
-            for word in splitted_line:
-                #print word
-                if word in n_grams_frequency.keys():
-                    n_grams_frequency[word] += 1
-                else:
-                    n_grams_frequency[word] = 1
-            
-    return n_grams_frequency
+	"""
+	reads file_in line by line.
+	
+	returns: map of n-grams with frequency count
+	"""
+	n_grams_frequency = {}    
+	if n < 1:
+		return n_grams_frequency
+	with open(file_in) as f:
+		corpus = ''
+		for line in f:
+			corpus +=  line
+			corpus += ' ' 
+		splitted_line = corpus.split()
+		if n > 1:
+			splitted_line = make_grams(splitted_line, n)
+		for word in splitted_line:
+			#print word
+			if word in n_grams_frequency.keys():
+				n_grams_frequency[word] += 1
+			else:
+				n_grams_frequency[word] = 1
+	return n_grams_frequency
     
 def print_ngrams(n_grams, m = None):
     idx = 0
