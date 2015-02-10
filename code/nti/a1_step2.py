f3ba5d8b540e5f783b4613b3c1a6d004353d4e4e
markus <mama@mia.it>
2015-02-10 22:18:35 +0100

    Q 2 done, i guess

--- code/nti/a1_step2.py
+++ code/nti/a1_step2.py
@@ -10,29 +10,77 @@ from start_stop_adder import add_start_stop
 from a1_step1 import parse_ngrams, print_ngrams, sort_ngrams
 from argparse import ArgumentParser
 from collections import Counter
+from sys import exit
 import itertools
 
 #%%
-def calc_probability_seq(seq, n_grams, n_min_1_grams):
-    words = seq.split()
+def rel_prob(w_all, n_grams):
+    """ 
+    calculates relative probability of sentence w_all given histogram n_grams
+    """
     
-    # get w_n word
-    w_n = words[-1]
+    s = sum(n_grams.values())
+    # divison by zero
+    if not s:
+        return -1
+    p_all = n_grams[' '.join(w_all)]
+    return float(p_all) / s
+
+#%%
+def cond_prob(w_all, w_rest, n_grams, n_min_1_grams):
+    """
+    calculates conditional probability of w_all|w_rest given histogram n_grams
+    and n_min_1_grams
     
-    # P(w_n|w_1,w)2,...,w_{n-1}) = P(w_1, ..., w_n) / P(w_1, ..., w_{n-1})
+    P(w_n|w_1,w)2,...,w_{n-1}) = P(w_1, ..., w_n) / P(w_1, ..., w_{n-1})
+    """
+
+    p_all = n_grams[' '.join(w_all)]
+    p_rest = n_min_1_grams[' '.join(w_rest)]
     
+    # avoid division by zero
+    if not p_rest:
+        return -1
+    
+    return float(p_all) / p_rest
     
 #%%
-def calc_probabilities_cond_file(cond_file, n_grams, n_min_1_grams):
+def calc_probabilities_cond_file(cond_file, n, n_grams, n_min_1_grams):
+    """
+    calculates all probabilities for cond_file given n_grams and n_min_1_grams
+    considers only sentences of length n
+    """
     with open(cond_file) as f:
-        return [calc_probability_seq(seq, n_grams, n_min_1_grams) for seq in f]
+        # dictionary for storing 'sentence' -> probability mapping
+        probs = {}
+        for seq in f:
+            # only consider sentences of length n
+            words = seq.split()
+            if len(words) is not n:
+                continue
+            p = -1
+            # conditional prob is only possible when n > 1. P(x|a)
+            # so when n == 0 -> calculate relative probabilty
+            if n > 1:
+                p = cond_prob(words, words[0:-1], n_grams, n_min_1_grams)
+            else:
+                p = rel_prob(words, n_grams)
+            probs[seq.strip()] = p
+            
+        return probs
 
 #%%
 def get_sentences(ss):
+    """
+    parses from START to STOP and puts every sentence in a list
+    """
     sentences = []    
     line_accum = []
     for l in ss:
+        # put current line on accumulator
         line_accum.append(l.split())
+        # we have a stop line, thus we flatten accumulator list and put stuff
+        # into sentences list
         if l[-5:-1] == 'STOP':
             sentences.append(list(itertools.chain(*line_accum)))
             line_accum = []
@@ -50,21 +98,26 @@ if __name__ == "__main__":
     parser.add_argument('-conditional-prob-file', dest='cond_file', type=str, help='file for conditional probabilities')
     args = parser.parse_args()
     
-    # get list of lines with START and STOP added
-    ss = add_start_stop(args.input_file)
+    if not args.input_file or not args.n:
+        parser.print_help()
+        exit(1)
+    
     # split and flatten array
     # sentences is list of sentences that start with START and end with STOP
-    sentences = get_sentences(ss)
-
-    def update_in_place(a,b):
-        a.update(b)
-        return a
+    sentences = get_sentences(add_start_stop(args.input_file))
 
     # this is fastest way that I have found to merge Counters
     # not that is necessary to avoid bigram STOP START. We want
     # to consider each sentence solely
-    n_grams = reduce(update_in_place, [parse_ngrams(sen, args.n) for sen in sentences])
-    n_min_1_grams = reduce(update_in_place, [parse_ngrams(sen, args.n - 1) for sen in sentences])
+    n_grams = reduce(lambda a,b: a.update(b) or a, 
+                     [parse_ngrams(sen, args.n) for sen in sentences])
+    n_min_1_grams = reduce(lambda a,b: a.update(b) or a,
+                           [parse_ngrams(sen, args.n - 1) for sen in sentences])
+
+    # when n=1 n_min_1_grams would become a dict instead of a Counter. To keep
+    # stuff consistent...
+    if not n_min_1_grams:
+        n_min_1_grams = Counter()
 
     # when conditional prob file is NOT given, print 10 most bigrams
     if args.m:
@@ -74,6 +127,7 @@ if __name__ == "__main__":
         print_ngrams(sort_ngrams(n_min_1_grams), args.m)
         
     if args.cond_file:
-        calc_probabilities_cond_file(args.cond_file, n_grams, n_min_1_grams)
+        probs = calc_probabilities_cond_file(args.cond_file, args.n, n_grams, n_min_1_grams)
+        print(probs)
     
     
