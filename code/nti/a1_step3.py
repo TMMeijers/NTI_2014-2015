bf80c1cca8fc40b0dd7c5d0b7ac994ffaba196d7
Cornelis Boon <killazelda@gmail.com>
2015-02-22 17:15:32 +0100

    New add1

--- code/nti/a1_step3.py
+++ code/nti/a1_step3.py
@@ -8,34 +8,65 @@ Created on Wed Feb 18 11:41:40 2015
 from __future__ import print_function
 from start_stop_adder import add_start_stop
 from a1_step1 import parse_ngrams, print_ngrams
-from a1_step2 import get_sentences, calc_probabilities_seq_file
+from a1_step2 import get_sentences, product, rel_prob, product
 from argparse import ArgumentParser
 from collections import Counter, OrderedDict
 from sys import exit
-from itertools import product, chain
+from itertools import chain
 
 def sort_ngrams_bidirectional(ngrams, order):
     return OrderedDict(sorted(ngrams.items(), key=lambda x: x[1], reverse=order))
 
 #%%
-def add_labda_smoothing(test_sentences, n_grams, n_min_1_grams, n):
+def seq_prob_add1(w_all, n, n_grams, n_min_1_grams, unigrams):
     """
     Applies add-1 smoothing to the bi-gram model
     """
     # As in assignment: assume V = unique words in train corpus, e.g. length of
     # n_min_1_grams for n = 2
-    V = len(n_min_1_grams)
-    return {' '.join(w_all): add_labda_prob(w_all, n_grams, n_min_1_grams, n, V) for w_all in test_sentences}
-        
-def add_labda_prob(w_all, n_grams, n_min_1_grams, n, V):
+    m = n -1 # if n > 1 else n    
+    # add START and STOPS
+    for i in xrange(0, m):
+        w_all.insert(0, 'START')
+        w_all.append('STOP')
+    parsed_n_grams = parse_ngrams(w_all, n)
+    if n is 1:
+        return product([rel_prob(ng.split(), n_grams) for ng in parsed_n_grams])
+    
+    return product([cond_prob_add1(ng.split(), ng.split()[0:-1], n_grams, n_min_1_grams, unigrams) for ng in parsed_n_grams])
+
+#%%
+def cond_prob_add1(w_all, w_rest, n_grams, n_min_1_grams, V):
     """
     Calculates the probability after add labda smoothing
     """
+    p_all = n_grams[''.join(w_all)]
+    p_rest = n_min_1_grams[''.join(w_rest)]
+    return (float(p_all)+1) / (p_rest + V)
+
+
+
+#%% Old add_labda function
+def add_labda_smoothing(test_sentences, n_grams, n_min_1_grams, n):
+"""
+Applies add-1 smoothing to the bi-gram model
+"""
+# As in assignment: assume V = unique words in train corpus, e.g. length of
+# n_min_1_grams for n = 2
+    V = len(n_min_1_grams)
+    return {' '.join(w_all) : add_labda_prob(w_all, n_grams, n_min_1_grams, n, V) for w_all in test_sentences}
+
+#%% 
+def add_labda_prob(w_all, n_grams, n_min_1_grams, n, V):
+"""
+Calculates the probability after add labda smoothing
+"""
     parsed_n_grams = parse_ngrams(w_all, n)
     prob = 0
     for ng in parsed_n_grams:
-            prob += float(n_grams[ng] + 1) / (1 + n_min_1_grams[' '.join(ng.split()[0])] + V)
+        prob *= float(n_grams[ng] + 1) / (V + n_min_1_grams[ng[0]])
     return prob
+
     
 #%%
 def good_turing_smoothing(test_sens, n_grams, n_min_1, n):
@@ -56,7 +87,8 @@ def good_turing_smoothing(test_sens, n_grams, n_min_1, n):
             n_grams[ng] = gt_smooth(n_grams[ng], N, k)    
     
     return {' '.join(w_all) : good_turing_prob(w_all, n_grams, n_min_1, n, k, N) for w_all in test_sens}
-    
+
+#%%    
 def good_turing_prob(w_all, ngrams, nmin1, n, k, N):
     """
     Returns the probability of sentences once the bi-grams have been smoothed
@@ -74,9 +106,19 @@ def good_turing_prob(w_all, ngrams, nmin1, n, k, N):
                 normalizer = 1
             prob += n_grams[ng]/normalizer
     return prob
-    
+#%% 
 def gt_smooth(c, N, k):    
     return float( ((c +1)* (N[c+1]/N[c]) - c*(((k+1)*N[k+1])/N[1]) )/(1 - (((k+1)*N[k+1])/N[1] )))
+
+#%%
+def calc_probabilities_seq_file(seq_file, n, n_grams, n_min_1_grams, unigrams, smoothing=None):
+    with open(seq_file) as f:
+        if smoothing is None:
+            return {seq.strip() : seq_prob(seq.split(), n, n_grams, n_min_1_grams) for seq in f}
+        if smoothing == 'add1':
+            return {seq.strip() : seq_prob_add1(seq.split(), n, n_grams, n_min_1_grams, unigrams) for seq in f}
+        if smoothing == 'gt':
+            return {seq.strip() : seq_prob_gt(seq.split(), n, n_grams, n_min_1_grams, unigrams) for seq in f}
 
 
 #%%
@@ -101,21 +143,19 @@ if __name__ == "__main__":
     
     n_grams = Counter(list(chain(*[parse_ngrams(sen, args.n) for sen in sentences])))
     n_min_1_grams = Counter(list(chain(*[parse_ngrams(sen, args.n - 1) for sen in sentences])))   
-        
-    if args.smoothing:
-        test_sentences = get_sentences(add_start_stop(args.test_file, args.n if not args.m else 1))        
-        if args.smoothing == 'add1':
-            probs = add_labda_smoothing(test_sentences, n_grams, n_min_1_grams, args.n)
-        elif args.smoothing == 'gt':
-            probs = good_turing_smoothing(test_sentences, n_grams, n_min_1_grams, args.n)        
-        percentagenonzero = 100 *len([prob for    prob in probs if probs[prob] != 0])/len(probs)
-        print('{} % of {} have a nonzero probability'.format(percentagenonzero, len(probs)))        
-        print('{} most likely sentences:'.format(args.m))
-        print_ngrams(sort_ngrams_bidirectional(probs, True), args.m)
-        print('{} least likely sentences:'.format(args.m))
-        print_ngrams(sort_ngrams_bidirectional(probs, False), args.m)
-        
+    
+    
+    if args.n > 2:        
+        unigrams = len(Counter(list(chain(*[parse_ngrams(sen, 1) for sen in sentences]))))
     else:
-        probs = calc_probabilities_seq_file(args.test_file, args.n, n_grams, n_min_1_grams)
-        print(probs)
-        exit()
+        unigrams = len(n_min_1_grams)
+    
+    probs = calc_probabilities_seq_file(args.test_file, args.n, n_grams, n_min_1_grams, unigrams, args.smoothing)        
+    
+    percentagenonzero = 100 *len([prob for    prob in probs if probs[prob] != 0])/len(probs)
+    print('{} % of {} have a nonzero probability'.format(percentagenonzero, len(probs)))        
+    print('{} most likely sentences:'.format(args.m))
+    print_ngrams(sort_ngrams_bidirectional(probs, True), args.m)
+    print('{} least likely sentences:'.format(args.m))
+    print_ngrams(sort_ngrams_bidirectional(probs, False), args.m)
+        
