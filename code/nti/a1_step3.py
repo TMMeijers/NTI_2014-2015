32ce6f7db1011d259c0e5c60975362c5f1355532
Cornelis Boon <killazelda@gmail.com>
2015-02-22 16:26:22 +0100

    Updated good_turing_smoothing. Now n_grams are only smoothed once

--- code/nti/a1_step3.py
+++ code/nti/a1_step3.py
@@ -8,7 +8,7 @@ Created on Wed Feb 18 11:41:40 2015
 from __future__ import print_function
 from start_stop_adder import add_start_stop
 from a1_step1 import parse_ngrams, print_ngrams
-from a1_step2 import get_sentences
+from a1_step2 import get_sentences, calc_probabilities_seq_file
 from argparse import ArgumentParser
 from collections import Counter, OrderedDict
 from sys import exit
@@ -34,7 +34,7 @@ def add_labda_prob(w_all, n_grams, n_min_1_grams, n, V):
     parsed_n_grams = parse_ngrams(w_all, n)
     prob = 0
     for ng in parsed_n_grams:
-            prob += float(n_grams[ng] + 1) / (V + n_min_1_grams[' '.join(ng.split()[0])])
+            prob += float(n_grams[ng] + 1) / (1 + n_min_1_grams[' '.join(ng.split()[0])] + V)
     return prob
     
 #%%
@@ -46,38 +46,36 @@ def good_turing_smoothing(test_sens, n_grams, n_min_1, n):
     Nzero = len(n_min_1_grams)**2 - len(n_grams)
     #Upper bound for smoothing    
     k = 5    
-    return {' '.join(w_all) : good_turing_prob(w_all, n_grams, n_min_1, n, k, Nzero) for w_all in test_sens}
-    
-def good_turing_prob(w_all, ngrams, nmin1, n, k, Nzero):
-    """
-    Applies gt-smoothing and returns the probability
-    """
-    parsed_n_grams = parse_ngrams(w_all, n)
-    prob = 0
     # Frequency of frequencies for frequencies of 0 to 6
     N = {i : len([ngram for ngram in n_grams.values() if ngram is i]) for i in xrange(k+2) if i is not 0}
     N[0] = Nzero
     
-    #Smoothe the bi-gram model    
-    for ng in parsed_n_grams:
+    #Smoothe the bi-gram model 
+    for ng in n_grams:
         if  n_grams[ng] < 6 and n_grams[ng] > 0:
-            
-            n_grams[ng] = gt_smooth(ng, n_grams, N, k)
-        else:
-            continue
+            n_grams[ng] = gt_smooth(n_grams[ng], N, k)    
     
+    return {' '.join(w_all) : good_turing_prob(w_all, n_grams, n_min_1, n, k, N) for w_all in test_sens}
+    
+def good_turing_prob(w_all, ngrams, nmin1, n, k, N):
+    """
+    Returns the probability of sentences once the bi-grams have been smoothed
+    """
+    parsed_n_grams = parse_ngrams(w_all, n)
+    prob = 0
+
     #Calculate the probabilities    
     for ng in parsed_n_grams:
         if ng not in n_grams:
             prob += N[1]/(N[0]*len(n_grams))
         else:
-            
-            prob += n_grams[ng]/sum(n_grams[ngram] for ngram in n_grams if ngram.split()[0] == ng.split()[0])
-    print(prob)
+            normalizer = sum(n_grams[ngram] for ngram in n_grams if ngram.split()[0] == ng.split()[0])
+            if normalizer == 0:
+                normalizer = 1
+            prob += n_grams[ng]/normalizer
     return prob
     
-def gt_smooth(ng, n_grams, N, k):
-    c = n_grams[ng]    
+def gt_smooth(c, N, k):    
     return float( ((c +1)* (N[c+1]/N[c]) - c*(((k+1)*N[k+1])/N[1]) )/(1 - (((k+1)*N[k+1])/N[1] )))
 
 
@@ -99,21 +97,25 @@ if __name__ == "__main__":
     # split and flatten array
     # sentences is list of sentences that start with START and end with STOP
     sentences = get_sentences(add_start_stop(args.train_file, args.n if not args.m else 1))
-    test_sentences = get_sentences(add_start_stop(args.test_file, args.n if not args.m else 1))
     
     
     n_grams = Counter(list(chain(*[parse_ngrams(sen, args.n) for sen in sentences])))
     n_min_1_grams = Counter(list(chain(*[parse_ngrams(sen, args.n - 1) for sen in sentences])))   
         
     if args.smoothing:
+        test_sentences = get_sentences(add_start_stop(args.test_file, args.n if not args.m else 1))        
         if args.smoothing == 'add1':
-           probs = add_labda_smoothing(test_sentences, n_grams, n_min_1_grams, args.n)
+            probs = add_labda_smoothing(test_sentences, n_grams, n_min_1_grams, args.n)
         elif args.smoothing == 'gt':
-            good_turing_smoothing(test_sentences, n_grams, n_min_1_grams, args.n)        
+            probs = good_turing_smoothing(test_sentences, n_grams, n_min_1_grams, args.n)        
+        percentagenonzero = 100 *len([prob for    prob in probs if probs[prob] != 0])/len(probs)
+        print('{} % of {} have a nonzero probability'.format(percentagenonzero, len(probs)))        
         print('{} most likely sentences:'.format(args.m))
         print_ngrams(sort_ngrams_bidirectional(probs, True), args.m)
         print('{} least likely sentences:'.format(args.m))
         print_ngrams(sort_ngrams_bidirectional(probs, False), args.m)
         
     else:
-        True
+        probs = calc_probabilities_seq_file(args.test_file, args.n, n_grams, n_min_1_grams)
+        print(probs)
+        exit()
